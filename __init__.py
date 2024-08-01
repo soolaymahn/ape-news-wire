83c143a18797820728768b6b6a2469393aa98505
Mohamedali Basrai <mohamedalibasrai@gmail.com>
2024-07-31 20:36:25 -0400

    update plan

--- __init__.py
+++
@@ -1,3 +0,0 @@
-# Step 1: query news API
-# Step 2: match news with memecoins using AI 
-# Step 3: serve data via a websocket
---
+++ python/news.py
@@ -0,0 +1,3 @@
+# Step 1: store set of token being tracked
+# Step 2: generate fake news stories for the tokens every few hours
+# Step 3: serve data via a websocket and/or send message via TG bot
